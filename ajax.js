4bcf3ce54c71dad2c1834769d6e15f49ed3b17bd
zmcdaniel <zmcdaniel@gmail.com>
2017-04-11 14:12:26 -0700

    js complete, working on styling

--- ajax.js
+++ ajax.js
@@ -1,11 +1,31 @@
 $(document).ready(function(){
-   $('#search-form').on('submit', search);
+   $('#search-button').on('click', search);
 });
 
 function search(e) {
+    console.log("Request fired!");
+
     e.preventDefault();
     clearPreviousSearchResults();
 
+    var myQuery = $('#query').val() || $('#query').attr("placeholder"),
+        url = "https://www.reddit.com/search.json";
+
+    $.get(url, {
+        q: myQuery
+    }).done(function(responseData){
+
+        var searchResults = responseData.data.children;
+        console.log(searchResults);
+        console.log(typeof searchResults);
+
+        for (var key in searchResults) {
+            addNewSearchResults(searchResults[key].data);
+        }
+
+    }).fail(function(error){
+        console.log("Something went wrong:", error.responseText.message);
+    });
 
 }
 
@@ -16,6 +36,17 @@ function clearPreviousSearchResults(){
     $("#results").html("");
 }
 
-function addNewSearchResults(){
+function addNewSearchResults(resultObject){
+    var newDiv = document.createElement("div"), link = document.createElement("a"), image = document.createElement("img");
 
+    newDiv.addClass("card");
+
+    link.href = resultObject.url;
+    link.textContent = resultObject.title;
+
+
+
+    $(li).append(link);
+
+    $("#results").append(li);
 }
